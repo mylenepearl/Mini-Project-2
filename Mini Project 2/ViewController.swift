52a12b63ef91b7c82b617ee9a98369221600f8a2
Mylene <marananmylene2@gmail.com>
2021-07-15 13:17:20 -0700

    Initial Commit

---
+++ Mini Project 2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mini Project 2
+//
+//  Created by Mylene Maranan on 7/15/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
